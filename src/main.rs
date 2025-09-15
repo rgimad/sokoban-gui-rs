d22f94d6aa7812b78fa7e1fb371d4b5888600127
rgimad <33692565+rgimad@users.noreply.github.com>
2025-09-16 01:08:02 +0300

    render more cells, refactor code

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use macroquad::prelude::*;
 /*
     # - wall
     . - floor
-    $ - $ - container
+    $ - box
     ~ - target
     @ - player
-    * - container on target
+    * - box on target
     + - player on target
 */
 
@@ -31,29 +31,48 @@ async fn main() {
         "#######"
     ];
 
-    let box_texture = Texture2D::from_file_with_format( include_bytes!("../assets/sprites/yoshi-32-box.png"), None);
+    let texture_wall = Texture2D::from_file_with_format( include_bytes!("../assets/sprites/yoshi-32-wall.png"), None);
+    //texture_box.set_filter(FilterMode::Nearest);
+    let texture_floor = Texture2D::from_file_with_format( include_bytes!("../assets/sprites/yoshi-32-floor.png"), None);
+    let texture_target = Texture2D::from_file_with_format( include_bytes!("../assets/sprites/yoshi-32-dock.png"), None);
+    let texture_box = Texture2D::from_file_with_format( include_bytes!("../assets/sprites/yoshi-32-box.png"), None);
+    let texture_player = Texture2D::from_file_with_format( include_bytes!("../assets/sprites/yoshi-32-worker.png"), None);
 
-    println!("{} {}", box_texture.width(), box_texture.height());
-    
-    box_texture.set_filter(FilterMode::Nearest);
+    println!("{} {}", texture_wall.width(), texture_wall.height());
 
     // TODO render level with sprites
     // Firstly, do this in main, after that move this logic to a separate function
     
     loop {
         clear_background(WHITE);
-        
-        // draw_texture(&texture, 50.0, 70.0, WHITE);
+
+        const START_X: f32 = 50.;
+        const START_Y: f32 = 70.;
 
         for (row_idx, row_str) in level.iter().enumerate() {
             for (col_idx, cell_char) in row_str.chars().enumerate() {
+                let mut cell_texture = &texture_floor;
                 match cell_char {
                     '#' => {
-                        draw_texture(&box_texture, 50. + col_idx as f32 * box_texture.width(), 70.0 + row_idx as f32 * box_texture.height(), WHITE);
+                        cell_texture = &texture_wall;
                     },
+                    '.' => {
+                        cell_texture = &texture_floor;
+                    },
+                    '~' => {
+                        cell_texture = &texture_target;
+                    },
+                    '$' => {
+                        cell_texture = &texture_box;
+                    },
+                    '@' => {
+                        cell_texture = &texture_player;
+                    },
+                    // TODO other types of cells
                     _ => {},
                 }
-            }
+                draw_texture(cell_texture, START_X + col_idx as f32 * texture_wall.width(), START_Y + row_idx as f32 * texture_wall.height(), WHITE);
+            } 
         }
         
         next_frame().await;
