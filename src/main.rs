15caaa08c8b605b926933f7d9c91e889382953e2
rgimad <33692565+rgimad@users.noreply.github.com>
2025-09-15 22:20:52 +0300

    draw boxes

--- src/main.rs
+++ src/main.rs
@@ -31,9 +31,11 @@ async fn main() {
         "#######"
     ];
 
-    let texture = Texture2D::from_file_with_format( include_bytes!("../assets/sprites/yoshi-32-box.png"), None);
+    let box_texture = Texture2D::from_file_with_format( include_bytes!("../assets/sprites/yoshi-32-box.png"), None);
+
+    println!("{} {}", box_texture.width(), box_texture.height());
     
-    texture.set_filter(FilterMode::Nearest);
+    box_texture.set_filter(FilterMode::Nearest);
 
     // TODO render level with sprites
     // Firstly, do this in main, after that move this logic to a separate function
@@ -41,7 +43,18 @@ async fn main() {
     loop {
         clear_background(WHITE);
         
-        draw_texture(&texture, 50.0, 70.0, WHITE);
+        // draw_texture(&texture, 50.0, 70.0, WHITE);
+
+        for (row_idx, row_str) in level.iter().enumerate() {
+            for (col_idx, cell_char) in row_str.chars().enumerate() {
+                match cell_char {
+                    '#' => {
+                        draw_texture(&box_texture, 50. + col_idx as f32 * box_texture.width(), 70.0 + row_idx as f32 * box_texture.height(), WHITE);
+                    },
+                    _ => {},
+                }
+            }
+        }
         
         next_frame().await;
     }
